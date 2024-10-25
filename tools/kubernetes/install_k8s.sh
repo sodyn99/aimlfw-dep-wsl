de53a1ab3446dae6486d7bb19278f09eac75e80e
Sungjin Lee <sodyn99@gmail.com>
2024-10-25 19:34:32 +0900

    Update for wsl(minikube)

--- tools/kubernetes/install_k8s.sh
+++ tools/kubernetes/install_k8s.sh
@@ -15,6 +15,16 @@
 #   limitations under the License.
 #
 # ==================================================================================
+
+check_status() {
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32m✓ $1 successful\e[0m"
+    else
+        echo -e "\e[31m✗ $1 failed\e[0m"
+        exit 1
+    fi
+}
+
 echo "Step 1: Disabling swap memory..."
 sudo swapoff -a
 sudo sed -i '/swap/s/^/#/' /etc/fstab
@@ -31,45 +41,50 @@ net.ipv4.ip_forward                 = 1
 EOF
 
 sudo sysctl --system
+check_status "Enabling IPv4 packet forwarding and loading kernel modules"
 
 echo "Step 3: Installing Containerd..."
 sudo apt update
 sudo apt install -y containerd
+check_status "Installing containerd"
+
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
+check_status "Configuring and restarting containerd"
 
-echo "Step 4: Installing Kubernetes packages..."
-sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl
+echo "Step 4: Downloading and installing minikube..."
+
+sudo apt update && sudo apt install -y conntrack containernetworking-plugins apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 sudo apt update && sudo apt install -y kubeadm=1.28.0-1.1 kubelet=1.28.0-1.1 kubectl=1.28.0-1.1
 sudo apt-mark hold kubelet kubeadm kubectl
 
-echo "Step 5: Initializing Kubernetes..."
-sudo kubeadm init
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
 
-echo "Removing taints from control-plane nodes..."
-for node in $(kubectl get nodes --no-headers | awk '{print $1}')
-do
-  echo "Removing taint from $node..."
-  kubectl taint nodes $node node-role.kubernetes.io/control-plane- --ignore-not-found=true
-done
+check_status "Installing kubeadm, kubelet, and kubectl"
 
-echo "Downloading and applying Calico..."
-curl -fsSL https://projectcalico.docs.tigera.io/manifests/calico.yaml -o calico.yaml
+VERSION="v1.28.0"
+curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm crictl-$VERSION-linux-amd64.tar.gz
+check_status "Installing crictl"
 
-echo "Modifying Calico configuration..."
-sudo sed -i 's/apiVersion: policy\/v1beta1/apiVersion: policy\/v1/g' calico.yaml
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
+check_status "Installing minikube"
 
-echo "Applying modified Calico configuration..."
-kubectl apply -f calico.yaml
+echo "Step 5: Starting minikube with Containerd and enabling ingress addon..."
+minikube start --driver=none --container-runtime=containerd --kubernetes-version=v1.28.0 \
+    --apiserver-ips 127.0.0.1 --apiserver-name localhost \
+    --addons=nvidia-device-plugin \
+    --cni=flannel
+check_status "Starting minikube with containerd runtime and enabling ingress"
 
-echo "Installation completed for kubernetes!"
+echo "Kubernetes cluster started with minikube using containerd!"
 
 # install nerdctl
 NERDCTL_VERSION=1.7.6 # see https://github.com/containerd/nerdctl/releases for the latest release
@@ -77,7 +92,7 @@ NERDCTL_VERSION=1.7.6 # see https://github.com/containerd/nerdctl/releases for t
 archType="amd64"
 if test "$(uname -m)" = "aarch64"
 then
-            archType="arm64"
+    archType="arm64"
 fi
 
 wget -q "https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-${NERDCTL_VERSION}-linux-${archType}.tar.gz" -O /tmp/nerdctl.tar.gz
@@ -88,23 +103,21 @@ echo "Installation completed for nerdctl!"
 # install buildkit
 BUILDKIT_VERSION=0.13.2 # see https://github.com/moby/buildkit/releases for the latest release
 
-archType="amd64"
-if test "$(uname -m)" = "aarch64"
-then
-            archType="arm64"
-fi
-echo "https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-${archType}.tar.gz"
 wget -q "https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-${archType}.tar.gz" -O /tmp/buildkit.tar.gz
 tar Cxzvvf /tmp /tmp/buildkit.tar.gz
 sudo mv /tmp/bin/buildctl /usr/bin/
+check_status "Installing buildkit"
 
 # run buildkit instance
 sudo nerdctl run -d --name buildkitd --privileged moby/buildkit:latest
+check_status "Running buildkitd instance"
 
 # install kustomize
 KUSTOMIZE_VERSION=5.4.2
-curl -LO "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz" 
-tar -xvzf "kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz" 
-sudo mv kustomize /usr/local/bin/ 
+curl -LO "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
+tar -xvzf "kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
+sudo mv kustomize /usr/local/bin/
 rm "kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz"
-echo "Kustomize installed successfully." 
+check_status "Installing kustomize"
+
+echo "Kustomize installed successfully."
