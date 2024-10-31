14907dc70ffa1af373a8bf94cb53f69143186ff6
Sungjin Lee <sodyn99@gmail.com>
2024-10-31 19:49:04 +0900

    Update install_anaconda.sh

--- bin/install_anaconda.sh
+++ bin/install_anaconda.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 check_status() {
     if [ $? -eq 0 ]; then
         echo -e "\e[32m✓ $1 successful\e[0m"
@@ -13,25 +11,29 @@ ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64
 INSTALLER="Anaconda3-2024.06-1-Linux-x86_64.sh"
 INSTALL_DIR="$HOME/anaconda3"
 
+echo "Starting Anaconda installation..."
 wget -q $ANACONDA_URL -O $INSTALLER
 check_status "Downloading Anaconda"
 
 chmod +x $INSTALLER
 
+echo "Installing Anaconda to $INSTALL_DIR..."
 ./$INSTALLER -b -p $INSTALL_DIR
+check_status "Anaconda installation"
 
+echo "Configuring conda environment..."
 source "$INSTALL_DIR/bin/activate"
 conda init
+check_status "Conda initialization"
 
 rm $INSTALLER
-check_status "Installing Anaconda"
 
+echo "Installing additional packages and configuring PATH..."
 conda install -y argcomplete
 echo 'export PATH=~/anaconda3/bin:~/anaconda3/condabin:$PATH' >> ~/.bashrc
 echo "eval \"$(register-python-argcomplete conda)\"" >> ~/.bashrc
 source ~/.bashrc
 conda config --set auto_activate_base false
-conda init
-source ~/.bashrc
+check_status "Anaconda configuration"
 
-check_status "Configuring Anaconda"
+echo "Anaconda setup completed successfully!"
